f8360799c3672e58a9975bb245c1c677f6048c7f
John <jddhalogamer@gmail.com>
2021-11-01 16:18:45 -0500

    added and styled Header, Balance, and Income-Expense components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpense from './components/IncomeExpense'
 
 const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
+      <div className='container'>
+      <Balance />
+      <IncomeExpense />
+      </div>
     </div>
   );
 }
