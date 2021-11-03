7963add0b679e889bf5f961caf12c8b784ad88c4
John <jddhalogamer@gmail.com>
2021-11-03 15:41:37 -0500

    added addTransaction to form.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const AddTransaction = () => {
     
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
 
+    const { addTransaction } = useContext(GlobalContext)
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000),
+            text,
+            amount: +amount
+        }
+        setText('')
+        setAmount('')
+        addTransaction(newTransaction)
+    }
+
     return (
         <div>
             <h3>Add Transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='form-control'>
                     <label htmlFor='text'>Text</label>
                     <input
