96b193a9fecfd0b152f45abb081db6868bd707a1
John <jddhalogamer@gmail.com>
2021-11-07 16:34:32 -0600

    deleteTransaction written to delete object by id.

--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -51,5 +51,27 @@ exports.addTransaction = async (req, res, next) => {
 
 //Delete Transaction/:id
 exports.deleteTransaction = async (req, res, next) => {
-    res.send('DELETE transaction');
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+
+        if(!transaction) {
+            return res.status(404).json({
+                success: false,
+                error: 'No transaction found'
+            });
+        }
+
+        await transaction.remove();
+
+        return res.status(200).json({
+            success: true,
+            data: {}
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        })
+    }
 }
